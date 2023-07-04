73fbcc27f7c93241f8f1f4f3337b4b7ecf3cbd54
celgost <brice.basty1@gmail.com>
2023-07-04 12:27:49 +0200

    ultra tiny refactor

--- exo_09.rb
+++ exo_09.rb
@@ -1,10 +1,8 @@
 puts "Ecris ton année de naissance (AAAA)"
 print "> "
 user_birth = gets.chomp.to_i
-date = 2023
 
-
-until user_birth > date
+until user_birth > date = 2023
   puts "#{user_birth}"
   user_birth += 1
 end
--- exo_10.rb
+++ exo_10.rb
@@ -2,9 +2,8 @@ puts "Ecris ton année de naissance (AAAA)"
 print "> "
 user_birth = gets.chomp.to_i
 user_age = 0
-date = 2023
 
-until user_birth > date
+until user_birth > date = 2023
   puts "Ton âge en #{user_birth} : #{user_age} an(s)"
   user_birth += 1
   user_age += 1
